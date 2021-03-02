25eb64e5f7c32580426d7b9d0847c5cd721a014b
Charles Coppieters 't Wallant <charles@Charless-MBP-2.fios-router.home>
2021-03-01 19:17:45 -0500

    My first commit

--- HandyRides/urls.py
+++ HandyRides/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rides/', include('rides.urls')),
-    path('', TemplateView.as_view(template_name="index.html"))
+    path('', TemplateView.as_view(template_name="index.html")),
+    path('about/', TemplateView.as_view(template_name="about.html"))
     
 ]
