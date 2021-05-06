b528b3afa40b2d5ce42e2a031aa9d51e0db65890
Charles Coppieters 't Wallant <charles@Charless-MBP-2.fios-router.home>
2021-05-06 01:10:22 -0400

    commit 39

--- buddies/urls.py
+++ buddies/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create', views.create, name='create'),
     path('addbuddy', views.addbuddy, name='addbuddy'),
-    path('feedback_email', views.feedback_email, name='feedback_email')
-    
+    #path('feedback_email', views.feedback_email, name='feedback_email')
 ]
